5d4e99f9484b616b83e673bc99acb128cf5ac1da
Timotej Valentin Rojko <timotejroiko@yahoo.com.br>
2020-08-15 21:34:33 +0100

    lint

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-let zlib = require("zlib");
-let methods = ["Inflate","Deflate","InflateRaw","DeflateRaw","Gzip","Gunzip","Unzip","BrotliDecompress","BrotliCompress"];
+"use strict";
+
+const zlib = require("zlib");
+const methods = ["Inflate","Deflate","InflateRaw","DeflateRaw","Gzip","Gunzip","Unzip","BrotliDecompress","BrotliCompress"];
 
 function lib(method,options = {}) {
 	method = method.toString();
 	method = method[0].toUpperCase() + method.substr(1);
 	if(!methods.includes(method)) { throw new Error("invalid or unsupported zlib class"); }
-	let brotli = method.startsWith("Brotli");
-	let unsafe = options.flush === true;
+	const brotli = method.startsWith("Brotli");
+	const unsafe = options.flush === true;
 	if(!Number.isInteger(options.flush)) { options.flush = brotli ? zlib.constants.BROTLI_OPERATION_FLUSH : zlib.constants.Z_SYNC_FLUSH; }
-	let z = new zlib[method](options);
-	let handle = z._handle;
-	let handleClose = z._handle.close;
-	let close = z.close;
+	const z = new zlib[method](options);
+	const { close, _handle, _handle: { close:handleClose } } = z;
 	let buffer = [];
-	let d = (data,f) => {
-		if(!Number.isInteger(f) && f !== true) { f = z._defaultFlushFlag; }
-		if(f!== true && !Buffer.isBuffer(data)) { data = Buffer.from(data); }
-		z._handle.close = () => {};
-		z.close = () => {};
+	const d = (data,f) => {
+		if(f !== true) {
+			if(!Number.isInteger(f)) { f = z._defaultFlushFlag; }
+			if(!Buffer.isBuffer(data)) { data = Buffer.from(data); }
+		}
+		z._handle.close = () => void 0;
+		z.close = () => void 0;
 		let result;
 		let error;
 		try {
 			result = z._processChunk(data, f);
 		} catch(e) {
 			error = e;
-		} finally {
-			z._handle = handle;
-			z._handle.close = handleClose;
-			z.close = close;
-			z.removeAllListeners("error");
-			if(error) {
-				z.reset();
-				throw error;
-			}
+		}
+		z._handle = _handle;
+		z._handle.close = handleClose;
+		z.close = close;
+		z.removeAllListeners("error");
+		if(error) {
+			z.reset();
+			throw error;
 		}
 		if(f !== true) {
-			result = Buffer.from(result)
+			result = Buffer.from(result);
 			if(f === (brotli ? zlib.constants.BROTLI_OPERATION_PROCESS : zlib.constants.Z_NO_FLUSH)) {
 				if(result.length) {
 					buffer.push(result);
@@ -48,7 +49,7 @@ function lib(method,options = {}) {
 			}
 		}
 		return result;
-	}
+	};
 	if(unsafe) { z._defaultFlushFlag = true; }
 	d._zlib = z;
 	return d;
