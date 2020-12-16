a502a597da9303961f17b2ad56553972eff29c83
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2020-12-16 20:03:02 +0000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 declare module "fast-zlib" {
     import * as zlib from "zlib";
     interface Instance {
-        (data: string | Buffer): Buffer;
+        (data: string | Buffer, flushFlag: number): Buffer;
         zlib: zlib.Zlib
     }
-    function lib(method: string, options: zlib.ZlibOptions): Instance;
+    function lib(method: string, options?: zlib.ZlibOptions): Instance;
     lib.constants = zlib.constants;
     export = lib;
 }
