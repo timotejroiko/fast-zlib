a7a526327ad409a429549da50e5e56586c1c98e7
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2020-12-21 23:52:56 +0000

    WIP v2

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,54 @@
-import * as zlib from "zlib";
+import {
+    ZlibOptions,
+    BrotliOptions,
+    Inflate as InflateClass,
+    Deflate as DeflateClass,
+    InflateRaw as InflateRawClass,
+    DeflateRaw as DeflateRawClass,
+    Gzip as GzipClass,
+    Gunzip as GunzipClass,
+    Unzip as UnzipClass,
+    BrotliCompress as BrotliCompressClass,
+    BrotliDecompress as BrotliDecompressClass
+} from "zlib";
 export { constants } from "zlib";
 
+declare abstract class ZlibBase {
+    constructor(options?: ZlibOptions);
+    process: (data: Buffer, flag?: number) => Buffer;
+}
+
+declare abstract class BrotliBase {
+    constructor(options?: BrotliOptions);
+    process: (data: Buffer, flag?: number) => Buffer;
+}
+
 declare module "fast-zlib" {
-    export class Inflate {
-        constructor(options?: zlib.ZlibOptions);
-        process: (data: Buffer, flag?: number) => Buffer;
-        instance: zlib.Inflate;
+    export class Inflate extends ZlibBase {
+        instance: InflateClass;
     }
-    export class Deflate {
-        constructor(options?: zlib.ZlibOptions);
-        process: (data: Buffer, flag?: number) => Buffer;
-        instance: zlib.Deflate;
+    export class Deflate extends ZlibBase {
+        instance: DeflateClass;
+    }
+    export class InflateRaw extends ZlibBase {
+        instance: InflateRawClass;
+    }
+    export class DeflateRaw extends ZlibBase {
+        instance: DeflateRawClass;
+    }
+    export class Gzip extends ZlibBase {
+        instance: GzipClass;
+    }
+    export class Gunzip extends ZlibBase {
+        instance: GunzipClass;
+    }
+    export class Unzip extends ZlibBase {
+        instance: UnzipClass;
+    }
+    export class BrotliCompress extends BrotliBase {
+        instance: BrotliCompressClass;
+    }
+    export class BrotliDecompress extends BrotliBase {
+        instance: BrotliDecompressClass;
     }
 }
